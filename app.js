3c32d4b454d2a7c07204c20f03677aa4fdccef29
Sean Namo <seannamo15@gmail.com>
2023-10-05 15:33:44 +0800

    Implemented all CRUD Operations for Coffee with External Routing

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const {sequelize, Coffee} = require('./models');
 
 const app = express();
 
+// exporting routes
+const coffee = require('./routes/Coffee');
+
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname +'/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const cart = [];
 
 // Home Route
 app.route('/')
@@ -23,75 +24,8 @@ app.route('/')
     });
 });
 
-// Create Route
-app.route('/create')
-.get(async (req, res) => {
-    res.render('create')
-})
-.post(async (req, res) => {
+app.use('/coffee', coffee)
 
-    coffeeDetails = req.body
-
-    const coffee = await Coffee.build(coffeeDetails);
-    await coffee.save();
-    console.log("Coffee saved successfully to DB!")
-    res.redirect('/')
-});
-
-// Update Route
-app.route('/update')
-.get(async (req, res) => {
-    const coffeeList = await Coffee.findAll();
-
-    res.render('update', {
-        coffee: coffeeList
-    });
-})
-
-app.route('/update/:coffeeName')
-.get(async (req, res) => {
-
-    const coffeeSelected = await Coffee.findAll({
-        where: {
-            name: req.params.coffeeName
-        }
-    });
-    console.log("Coffee selected:", coffeeSelected);
-    res.render('update-form', {
-        coffee: coffeeSelected
-    });
-})
-.post( async (req, res) => {
-    try {
-        await Coffee.update({ name: req.body.name }, {
-            where: {
-              name: req.params.coffeeName,
-            },
-          });
-
-          res.redirect('/');
-    } catch (error) {
-        res.send(error);
-    }
-});
-
-// Route to Delete
-app.route('/delete')
-.get( async(req, res) => {
-    res.render('delete');
-})
-.post(async (req, res) => {
-    try {
-        await Coffee.destroy({
-            where: {
-                name: req.body.name
-            }
-        })
-        res.redirect('/');
-    } catch (error) {
-        res.status(error.status);
-    }
-});
 
 app.listen(3000, async () => {
     console.log('Example app listening on port 3000!');
---
+++ routes/Coffee.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = express.Router();
+const { Coffee } = require('../models/');
+
+router.use(async (req, res, next) => {
+    console.log("Time:", Date.now());
+    next();
+})
+
+// Create Route
+router.get('/create', async (req, res) => {
+    res.render('create')
+})
+router.post('/create', async (req, res) => {
+
+    coffeeDetails = req.body
+
+    const coffee = await Coffee.build(coffeeDetails);
+    await coffee.save();
+    console.log("Coffee saved successfully to DB!");
+    res.redirect('/coffee/list');
+});
+
+// Update Route
+router.get('/list', async (req, res) => {
+    const coffeeList = await Coffee.findAll();
+
+    res.render('coffee-list', {
+        coffee: coffeeList
+    });
+})
+
+
+router.get('/list/:coffeeName', async (req, res) => {
+
+    const coffeeSelected = await Coffee.findAll({
+        where: {
+            name: req.params.coffeeName
+        }
+    });
+    console.log("Coffee selected:", coffeeSelected);
+    res.render('update-form', {
+        coffee: coffeeSelected
+    });
+})
+
+router.post('/list/:coffeeName', async (req, res) => {
+    try {
+        await Coffee.update({ 
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price
+        }, {
+            where: {
+              name: req.params.coffeeName,
+            },
+          });
+
+          res.redirect('/');
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+// Route to Delete
+router.post('/list/delete/:coffeeName', (async (req, res) => {
+    try {
+        await Coffee.destroy({
+            where: {
+                name: req.params.coffeeName
+            }
+        })
+        res.redirect('/coffee/list');
+    } catch (error) {
+        res.status(error.status);
+    }
+}));
+
+module.exports = router;
